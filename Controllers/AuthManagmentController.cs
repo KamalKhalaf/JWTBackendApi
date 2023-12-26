d5f7448f582a1f2f14cb87c21eee183e4d6fc8ce
Kamal.Farghali <Kamal.Farghali@procco.com>
2023-12-26 11:15:37 +0300

    Create Auth managent controller

---
+++ Controllers/AuthManagmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DriversApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthManagmentController : ControllerBase
+    {
+        private readonly ILogger<AuthManagmentController> _logger;
+        public AuthManagmentController(ILogger<AuthManagmentController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
